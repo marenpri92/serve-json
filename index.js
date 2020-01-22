3f5ad86f71f0d9d497f500ee57adcd99d803de77
marenpri <marnpri92@gmail.com>
2020-01-22 22:28:53 +0100

    commit initial

---
+++ index.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const express = require("express");
+var multer = require("multer");
+
+const app = express();
+
+var storage = multer.diskStorage({
+  destination: function(req, file, callback) {
+    callback(null, "./data");
+  },
+  filename: function(req, file, callback) {
+    callback(null, file.originalname);
+  }
+});
+
+var upload = multer({ storage: storage }).single("jsonfile");
+
+app.use(express.static("public"));
+
+app.get("/:file", (req, res) => {
+  const file = req.params["file"];
+  fs.readFile(__dirname + "/data/" + file + ".json", (err, data) => {
+    if (err) {
+      return res.status(404).json({ message: "File not found" });
+    }
+    const jsonData = JSON.parse(data);
+    res.status(200).json(jsonData);
+  });
+});
+
+app.post("/upload", function(req, res) {
+  upload(req, res, function(err) {
+    if (err) {
+      return res.end("Error uploading file.");
+    }
+    res.end("File is uploaded");
+  });
+});
+
+app.listen(8080);
